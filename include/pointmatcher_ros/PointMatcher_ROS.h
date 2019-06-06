190fba4120238335de82abdfad0bed314d7f2511
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2019-06-06 14:15:33 -0400

    Added a conversion function for sensor_msgs::LaserScan scans.

--- include/pointmatcher_ros/PointMatcher_ROS.h
+++ include/pointmatcher_ros/PointMatcher_ROS.h
@@ -1,11 +1,14 @@
 #include <pointmatcher/PointMatcher.h>
 #include <pointmatcher/IO.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/LaserScan.h>
 
 namespace PointMatcher_ROS
 {
 	template<typename T>
 	typename PointMatcher<T>::DataPoints rosMsgToPointMatcherCloud(const sensor_msgs::PointCloud2& rosMsg);
+	template<typename T>
+	typename PointMatcher<T>::DataPoints rosMsgToPointMatcherCloud(const sensor_msgs::LaserScan& rosMsg);
 
 	template<typename T>
 	sensor_msgs::PointCloud2 pointMatcherCloudToRosMsg(const typename PointMatcher<T>::DataPoints& pmCloud, const std::string& frame_id, const ros::Time& stamp);
--- src/PointMatcher_ROS.cpp
+++ src/PointMatcher_ROS.cpp
@@ -224,6 +224,86 @@ template
 PointMatcher<double>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud<double>(const sensor_msgs::PointCloud2& rosMsg);
 
 template<typename T>
+typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud(const sensor_msgs::LaserScan& rosMsg)
+{
+	typedef PointMatcher<T> PM;
+	typedef typename PM::DataPoints DataPoints;
+	typedef typename DataPoints::Label Label;
+	typedef typename DataPoints::Labels Labels;
+	typedef typename DataPoints::View View;
+
+	Labels featLabels;
+	featLabels.push_back(Label("x", 1));
+	featLabels.push_back(Label("y", 1));
+	featLabels.push_back(Label("pad", 1));
+
+	// Build descriptors
+	Labels descLabels;
+	if (!rosMsg.intensities.empty())
+	{
+		descLabels.push_back(Label("intensity", 1));
+		assert(rosMsg.intensities.size() == rosMsg.ranges.size());
+	}
+
+	// filter points based on range
+	std::vector<size_t> ids(rosMsg.ranges.size());
+	std::vector<double> ranges(rosMsg.ranges.size());
+	std::vector<double> intensities(rosMsg.intensities.size());
+
+	size_t goodCount(0);
+	for (size_t i = 0; i < rosMsg.ranges.size(); ++i)
+	{
+		const float range(rosMsg.ranges[i]);
+		if (range >= rosMsg.range_min && range <= rosMsg.range_max)
+		{
+			ranges[goodCount] = range;
+			ids[goodCount] = i;
+			if(!rosMsg.intensities.empty())
+			{
+				intensities[goodCount] = rosMsg.intensities[i];
+			}
+			++goodCount;
+		}
+	}
+
+	ids.resize(goodCount);
+	ranges.resize(goodCount);
+	if(!rosMsg.intensities.empty())
+	{
+		intensities.resize(goodCount);
+	}
+
+	DataPoints cloud(featLabels, descLabels, goodCount);
+	cloud.getFeatureViewByName("pad").setConstant(1);
+
+	// fill features
+	for (size_t i = 0; i < ranges.size(); ++i)
+	{
+		const T angle = rosMsg.angle_min + ids[i]*rosMsg.angle_increment;
+		const T range(ranges[i]);
+		const T x = cos(angle) * range;
+		const T y = sin(angle) * range;
+	}
+
+	// fill descriptors
+	if (!rosMsg.intensities.empty())
+	{
+		auto is(cloud.getDescriptorViewByName("intensity"));
+		for (size_t i = 0; i < intensities.size(); ++i)
+		{
+			is(0, i) = intensities[i];
+		}
+	}
+
+	return cloud;
+}
+
+template
+PointMatcher<float>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud<float>(const sensor_msgs::LaserScan& rosMsg);
+template
+PointMatcher<double>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud<double>(const sensor_msgs::LaserScan& rosMsg);
+
+template<typename T>
 sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg(const typename PointMatcher<T>::DataPoints& pmCloud, const std::string& frame_id, const ros::Time& stamp)
 {
 
