cde5c5adefab019bf22768e4ff7e517263ca6920
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2022-09-12 17:01:22 -0400

    It compiles on Humble.

--- include/pointmatcher_ros/PointMatcher_ROS.h
+++ include/pointmatcher_ros/PointMatcher_ROS.h
@@ -1,31 +1,32 @@
 #include <pointmatcher/PointMatcher.h>
 #include <pointmatcher/IO.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/LaserScan.h>
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/TransformStamped.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 namespace PointMatcher_ROS
 {
 	template<typename T>
-	typename PointMatcher<T>::DataPoints rosMsgToPointMatcherCloud(const sensor_msgs::PointCloud2& rosMsg);
+	typename PointMatcher<T>::DataPoints rosMsgToPointMatcherCloud(const sensor_msgs::msg::PointCloud2& rosMsg);
 	
 	template<typename T>
-	typename PointMatcher<T>::DataPoints rosMsgToPointMatcherCloud(const sensor_msgs::LaserScan& rosMsg);
-	
+	typename PointMatcher<T>::DataPoints rosMsgToPointMatcherCloud(const sensor_msgs::msg::LaserScan& rosMsg);
+
 	template<typename T>
-	sensor_msgs::PointCloud2 pointMatcherCloudToRosMsg(const typename PointMatcher<T>::DataPoints& pmCloud, const std::string& frame_id,
-													   const ros::Time& stamp);
-	
+	sensor_msgs::msg::PointCloud2 pointMatcherCloudToRosMsg(const typename PointMatcher<T>::DataPoints& pmCloud, const std::string& frame_id,
+													   const rclcpp::Time& stamp);
+
 	template<typename T>
-	nav_msgs::Odometry pointMatcherTransformationToOdomMsg(const typename PointMatcher<T>::TransformationParameters& inTr, const std::string& frame_id,
-														   const std::string& child_frame_id, const ros::Time& stamp);
-	
+	nav_msgs::msg::Odometry pointMatcherTransformationToOdomMsg(const typename PointMatcher<T>::TransformationParameters& inTr, const std::string& frame_id,
+														   const std::string& child_frame_id, const rclcpp::Time& stamp);
+
 	template<typename T>
-	typename PointMatcher<T>::TransformationParameters rosTfToPointMatcherTransformation(const geometry_msgs::TransformStamped& transformStamped,
+	typename PointMatcher<T>::TransformationParameters rosTfToPointMatcherTransformation(const geometry_msgs::msg::TransformStamped& transformStamped,
 																						 const int& transformationDimension);
-	
+
 	template<typename T>
-	geometry_msgs::TransformStamped pointMatcherTransformationToRosTf(const typename PointMatcher<T>::TransformationParameters& inTr,
-																	  const std::string& frame_id, const std::string& child_frame_id, const ros::Time& stamp);
+	geometry_msgs::msg::TransformStamped pointMatcherTransformationToRosTf(const typename PointMatcher<T>::TransformationParameters& inTr,
+																	  const std::string& frame_id, const std::string& child_frame_id, const rclcpp::Time& stamp);
 }
--- src/PointMatcher_ROS.cpp
+++ src/PointMatcher_ROS.cpp
@@ -1,10 +1,10 @@
 #include "pointmatcher_ros/PointMatcher_ROS.h"
 #include "utils.h"
-#include <tf2_eigen/tf2_eigen.h>
-#include <geometry_msgs/TransformStamped.h>
+#include <tf2_eigen/tf2_eigen.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 template<typename T>
-typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud(const sensor_msgs::PointCloud2& rosMsg)
+typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud(const sensor_msgs::msg::PointCloud2& rosMsg)
 {
 	typedef PointMatcher <T> PM;
 	typedef typename PointMatcherIO<T>::PMPropTypes PM_types;
@@ -13,7 +13,7 @@ typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud
 	typedef typename DataPoints::Labels Labels;
 	typedef typename DataPoints::View View;
 	typedef typename DataPoints::TimeView TimeView;
-	
+
 	Labels featLabels;
 	Labels descLabels;
 	Labels timeLabels;
@@ -62,7 +62,7 @@ typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud
 			std::string startingName = name;
 			erase_last(startingName, "_splitTime_high32");
 			const std::string beginning = startingName;
-			
+
 			timeLabels.push_back(Label(beginning, 1));
 			it += 1;
 			isFeature.push_back(false);
@@ -82,18 +82,18 @@ typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud
 			fieldTypes.push_back(PM_types::DESCRIPTOR);
 		}
 	}
-	
+
 	featLabels.push_back(Label("pad", 1));
 	assert(isFeature.size() == rosMsg.fields.size());
 	assert(fieldTypes.size() == rosMsg.fields.size());
-	
+
 	// create cloud
 	const unsigned pointCount(rosMsg.width * rosMsg.height);
 	DataPoints cloud(featLabels, descLabels, timeLabels, pointCount);
 	cloud.getFeatureViewByName("pad").setConstant(1);
-	
+
 	// fill cloud
-	typedef sensor_msgs::PointField PF;
+	typedef sensor_msgs::msg::PointField PF;
 	size_t fieldId = 0;
 	for(auto it(rosMsg.fields.begin()); it != rosMsg.fields.end(); ++it, ++fieldId)
 	{
@@ -144,9 +144,9 @@ typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud
 			{
 				erase_last(startingName, "_splitTime_low32");
 			}
-			
+
 			TimeView timeView(cloud.getTimeViewByName(startingName));
-			
+
 			int ptId(0);
 			const size_t count(std::max<size_t>(it->count, 1));
 			for(size_t y(0); y < rosMsg.height; ++y)
@@ -168,18 +168,18 @@ typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud
 							const uint32_t high32 = uint32_t(timeView(dim, ptId) >> 32);
 							const uint32_t low32 = *reinterpret_cast<const uint32_t*>(fPtr);
 							timeView(dim, ptId) = (((uint64_t)high32) << 32) | ((uint64_t)low32);
-							
+
 						}
 						dataPtr += rosMsg.point_step;
 						ptId += 1;
 					}
 				}
 			}
-			
+
 		}
 		else
 		{
-			
+
 			// get view for editing data
 			View view(
 					(it->name == "normal_x") ? cloud.getDescriptorRowViewByName("normals", 0) :
@@ -188,7 +188,7 @@ typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud
 					  ((isFeature[fieldId]) ? cloud.getFeatureViewByName(it->name) :
 					   cloud.getDescriptorViewByName(it->name))))
 			);
-			
+
 			// use view to read data
 			int ptId(0);
 			const size_t count(std::max<size_t>(it->count, 1));
@@ -249,25 +249,25 @@ typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud
 }
 
 template
-PointMatcher<float>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud<float>(const sensor_msgs::PointCloud2& rosMsg);
+PointMatcher<float>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud<float>(const sensor_msgs::msg::PointCloud2& rosMsg);
 
 template
-PointMatcher<double>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud<double>(const sensor_msgs::PointCloud2& rosMsg);
+PointMatcher<double>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud<double>(const sensor_msgs::msg::PointCloud2& rosMsg);
 
 template<typename T>
-typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud(const sensor_msgs::LaserScan& rosMsg)
+typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud(const sensor_msgs::msg::LaserScan& rosMsg)
 {
 	typedef PointMatcher <T> PM;
 	typedef typename PM::DataPoints DataPoints;
 	typedef typename DataPoints::Label Label;
 	typedef typename DataPoints::Labels Labels;
 	typedef typename DataPoints::View View;
-	
+
 	Labels featLabels;
 	featLabels.push_back(Label("x", 1));
 	featLabels.push_back(Label("y", 1));
 	featLabels.push_back(Label("pad", 1));
-	
+
 	// Build descriptors
 	Labels descLabels;
 	if(!rosMsg.intensities.empty())
@@ -275,12 +275,12 @@ typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud
 		descLabels.push_back(Label("intensity", 1));
 		assert(rosMsg.intensities.size() == rosMsg.ranges.size());
 	}
-	
+
 	// filter points based on range
 	std::vector <size_t> ids(rosMsg.ranges.size());
 	std::vector<double> ranges(rosMsg.ranges.size());
 	std::vector<double> intensities(rosMsg.intensities.size());
-	
+
 	size_t goodCount(0);
 	for(size_t i = 0; i < rosMsg.ranges.size(); ++i)
 	{
@@ -296,27 +296,27 @@ typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud
 			++goodCount;
 		}
 	}
-	
+
 	ids.resize(goodCount);
 	ranges.resize(goodCount);
 	if(!rosMsg.intensities.empty())
 	{
 		intensities.resize(goodCount);
 	}
-	
+
 	DataPoints cloud(featLabels, descLabels, goodCount);
 	cloud.getFeatureViewByName("pad").setConstant(1);
-	
+
 	// fill features
 	for(size_t i = 0; i < ranges.size(); ++i)
 	{
 		const T angle = rosMsg.angle_min + ids[i] * rosMsg.angle_increment;
 		const T range(ranges[i]);
-		
+
 		cloud.features(0, i) = cos(angle) * range;
 		cloud.features(1, i) = sin(angle) * range;
 	}
-	
+
 	// fill descriptors
 	if(!rosMsg.intensities.empty())
 	{
@@ -326,24 +326,24 @@ typename PointMatcher<T>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud
 			is(0, i) = intensities[i];
 		}
 	}
-	
+
 	return cloud;
 }
 
 template
-PointMatcher<float>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud<float>(const sensor_msgs::LaserScan& rosMsg);
+PointMatcher<float>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud<float>(const sensor_msgs::msg::LaserScan& rosMsg);
 
 template
-PointMatcher<double>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud<double>(const sensor_msgs::LaserScan& rosMsg);
+PointMatcher<double>::DataPoints PointMatcher_ROS::rosMsgToPointMatcherCloud<double>(const sensor_msgs::msg::LaserScan& rosMsg);
 
 template<typename T>
-sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg(const typename PointMatcher<T>::DataPoints& pmCloud, const std::string& frame_id,
-																	 const ros::Time& stamp)
+sensor_msgs::msg::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg(const typename PointMatcher<T>::DataPoints& pmCloud, const std::string& frame_id,
+																	 const rclcpp::Time& stamp)
 {
-	
-	sensor_msgs::PointCloud2 rosCloud;
-	typedef sensor_msgs::PointField PF;
-	
+
+	sensor_msgs::msg::PointCloud2 rosCloud;
+	typedef sensor_msgs::msg::PointField PF;
+
 	// check type and get sizes
 	assert(std::is_floating_point<T>::value);
 	assert(!(std::is_same<T, long double>::value));
@@ -359,11 +359,11 @@ sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg(const typen
 		dataType = PF::FLOAT64;
 		scalarSize = 8;
 	}
-	
+
 	size_t timeSize = 4; // we split in two UINT32
-	
+
 	// build labels
-	
+
 	// features
 	unsigned offset(0);
 	bool addZ(false);
@@ -397,7 +397,7 @@ sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg(const typen
 			addZ = true;
 		}
 	}
-	
+
 	// descriptors
 	const bool isDescriptor(!pmCloud.descriptorLabels.empty());
 	bool hasColor(false);
@@ -453,7 +453,7 @@ sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg(const typen
 		}
 		inDescriptorPos += it->span;
 	}
-	
+
 	// time
 	bool hasTime(false);
 	for(auto it(pmCloud.timeLabels.begin()); it != pmCloud.timeLabels.end(); ++it)
@@ -462,17 +462,17 @@ sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg(const typen
 		if(it->text == "time")
 		{
 			hasTime = true;
-			
+
 			// for Rviz view
-			
+
 			pointField.datatype = PF::FLOAT32;
-			
+
 			pointField.name = "elapsedTimeSec";
 			pointField.offset = offset;
 			pointField.count = 1;
 			rosCloud.fields.push_back(pointField);
 			offset += 4;
-			
+
 			// Split time in two because there is not PF::UINT64
 			pointField.datatype = PF::UINT32;
 			pointField.name = it->text + "_splitTime_high32";
@@ -480,7 +480,7 @@ sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg(const typen
 			pointField.count = 1;
 			rosCloud.fields.push_back(pointField);
 			offset += timeSize;
-			
+
 			pointField.datatype = PF::UINT32;
 			pointField.name = it->text + "_splitTime_low32";
 			pointField.offset = offset;
@@ -489,7 +489,7 @@ sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg(const typen
 			offset += timeSize;
 		}
 	}
-	
+
 	// fill cloud with data
 	rosCloud.header.frame_id = frame_id;
 	rosCloud.header.stamp = stamp;
@@ -500,20 +500,20 @@ sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg(const typen
 	rosCloud.row_step = rosCloud.point_step * rosCloud.width;
 	rosCloud.is_dense = true;
 	rosCloud.data.resize(rosCloud.row_step * rosCloud.height);
-	
+
 	const unsigned featureDim(pmCloud.features.rows() - 1);
 	const unsigned descriptorDim(pmCloud.descriptors.rows());
 	const unsigned timeDim(pmCloud.times.rows());
-	
+
 	assert(descriptorDim == inDescriptorPos);
 	const unsigned postColorPos(colorPos + colorCount);
 	assert(postColorPos <= inDescriptorPos);
 	const unsigned postColorCount(descriptorDim - postColorPos);
-	
+
 	for(unsigned pt(0); pt < rosCloud.width; ++pt)
 	{
 		uint8_t* fPtr(&rosCloud.data[pt * offset]);
-		
+
 		memcpy(fPtr, reinterpret_cast<const uint8_t*>(&pmCloud.features(0, pt)), scalarSize * featureDim);
 		fPtr += scalarSize * featureDim;
 		if(addZ)
@@ -551,54 +551,54 @@ sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg(const typen
 				fPtr += scalarSize * descriptorDim;
 			}
 		}
-		
+
 		if(hasTime)
 		{
 			const size_t ptrSize = timeSize * timeDim;
-			
+
 			// Elapsed time
 			const float elapsedTime = (float)(pmCloud.times(0, pt) - pmCloud.times(0, 0)) * 1e-9f;
 			memcpy(fPtr, reinterpret_cast<const uint8_t*>(&elapsedTime), ptrSize);
 			fPtr += ptrSize;
-			
+
 			// high32
 			const uint32_t high32 = (uint32_t)(pmCloud.times(0, pt) >> 32);
 			memcpy(fPtr, reinterpret_cast<const uint8_t*>(&high32), ptrSize);
 			fPtr += ptrSize;
-			
+
 			// low32
 			const uint32_t low32 = (uint32_t)(pmCloud.times(0, pt));
 			memcpy(fPtr, reinterpret_cast<const uint8_t*>(&low32), ptrSize);
 			fPtr += ptrSize;
-			
+
 		}
 	}
-	
+
 	// fill remaining information
 	rosCloud.header.frame_id = frame_id;
 	rosCloud.header.stamp = stamp;
-	
+
 	return rosCloud;
 }
 
 template
-sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg<float>(const PointMatcher<float>::DataPoints& pmCloud, const std::string& frame_id,
-																			const ros::Time& stamp);
+sensor_msgs::msg::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg<float>(const PointMatcher<float>::DataPoints& pmCloud, const std::string& frame_id,
+																			const rclcpp::Time& stamp);
 
 template
-sensor_msgs::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg<double>(const PointMatcher<double>::DataPoints& pmCloud, const std::string& frame_id,
-																			 const ros::Time& stamp);
+sensor_msgs::msg::PointCloud2 PointMatcher_ROS::pointMatcherCloudToRosMsg<double>(const PointMatcher<double>::DataPoints& pmCloud, const std::string& frame_id,
+																			 const rclcpp::Time& stamp);
 
 template<typename T>
-nav_msgs::Odometry PointMatcher_ROS::pointMatcherTransformationToOdomMsg(const typename PointMatcher<T>::TransformationParameters& inTr,
+nav_msgs::msg::Odometry PointMatcher_ROS::pointMatcherTransformationToOdomMsg(const typename PointMatcher<T>::TransformationParameters& inTr,
 																		 const std::string& frame_id, const std::string& child_frame_id,
-																		 const ros::Time& stamp)
+																		 const rclcpp::Time& stamp)
 {
-	nav_msgs::Odometry odom;
+	nav_msgs::msg::Odometry odom;
 	odom.header.stamp = stamp;
 	odom.header.frame_id = frame_id;
 	odom.child_frame_id = child_frame_id;
-	
+
 	// Fill pose
 	const Eigen::Affine3d eigenTr(
 			Eigen::Matrix4d(
@@ -608,7 +608,7 @@ nav_msgs::Odometry PointMatcher_ROS::pointMatcherTransformationToOdomMsg(const t
 			)
 	);
 	odom.pose.pose = tf2::toMsg(eigenTr);
-	
+
 	// Fill velocity
 	odom.twist.covariance[0 + 0 * 6] = -1;
 	odom.twist.covariance[1 + 1 * 6] = -1;
@@ -616,22 +616,22 @@ nav_msgs::Odometry PointMatcher_ROS::pointMatcherTransformationToOdomMsg(const t
 	odom.twist.covariance[3 + 3 * 6] = -1;
 	odom.twist.covariance[4 + 4 * 6] = -1;
 	odom.twist.covariance[5 + 5 * 6] = -1;
-	
+
 	return odom;
 }
 
 template
-nav_msgs::Odometry PointMatcher_ROS::pointMatcherTransformationToOdomMsg<float>(const PointMatcher<float>::TransformationParameters& inTr,
+nav_msgs::msg::Odometry PointMatcher_ROS::pointMatcherTransformationToOdomMsg<float>(const PointMatcher<float>::TransformationParameters& inTr,
 																				const std::string& frame_id, const std::string& child_frame_id,
-																				const ros::Time& stamp);
+																				const rclcpp::Time& stamp);
 
 template
-nav_msgs::Odometry PointMatcher_ROS::pointMatcherTransformationToOdomMsg<double>(const PointMatcher<double>::TransformationParameters& inTr,
+nav_msgs::msg::Odometry PointMatcher_ROS::pointMatcherTransformationToOdomMsg<double>(const PointMatcher<double>::TransformationParameters& inTr,
 																				 const std::string& frame_id, const std::string& child_frame_id,
-																				 const ros::Time& stamp);
+																				 const rclcpp::Time& stamp);
 
 template<typename T>
-typename PointMatcher<T>::TransformationParameters PointMatcher_ROS::rosTfToPointMatcherTransformation(const geometry_msgs::TransformStamped& transformStamped,
+typename PointMatcher<T>::TransformationParameters PointMatcher_ROS::rosTfToPointMatcherTransformation(const geometry_msgs::msg::TransformStamped& transformStamped,
 																									   const int& transformationDimension)
 {
 	Eigen::Affine3d eigenTr = tf2::transformToEigen(transformStamped);
@@ -640,16 +640,16 @@ typename PointMatcher<T>::TransformationParameters PointMatcher_ROS::rosTfToPoin
 
 template
 PointMatcher<float>::TransformationParameters
-PointMatcher_ROS::rosTfToPointMatcherTransformation<float>(const geometry_msgs::TransformStamped& transformStamped, const int& transformationDimension);
+PointMatcher_ROS::rosTfToPointMatcherTransformation<float>(const geometry_msgs::msg::TransformStamped& transformStamped, const int& transformationDimension);
 
 template
 PointMatcher<double>::TransformationParameters
-PointMatcher_ROS::rosTfToPointMatcherTransformation<double>(const geometry_msgs::TransformStamped& transformStamped, const int& transformationDimension);
+PointMatcher_ROS::rosTfToPointMatcherTransformation<double>(const geometry_msgs::msg::TransformStamped& transformStamped, const int& transformationDimension);
 
 template<typename T>
-geometry_msgs::TransformStamped PointMatcher_ROS::pointMatcherTransformationToRosTf(const typename PointMatcher<T>::TransformationParameters& inTr,
+geometry_msgs::msg::TransformStamped PointMatcher_ROS::pointMatcherTransformationToRosTf(const typename PointMatcher<T>::TransformationParameters& inTr,
 																					const std::string& frame_id, const std::string& child_frame_id,
-																					const ros::Time& stamp)
+																					const rclcpp::Time& stamp)
 {
 	const Eigen::Affine3d eigenTr(
 			Eigen::Matrix4d(
@@ -658,7 +658,7 @@ geometry_msgs::TransformStamped PointMatcher_ROS::pointMatcherTransformationToRo
 					)
 			)
 	);
-	geometry_msgs::TransformStamped tfTr = tf2::eigenToTransform(eigenTr);
+	geometry_msgs::msg::TransformStamped tfTr = tf2::eigenToTransform(eigenTr);
 	tfTr.header.frame_id = frame_id;
 	tfTr.child_frame_id = child_frame_id;
 	tfTr.header.stamp = stamp;
@@ -666,11 +666,11 @@ geometry_msgs::TransformStamped PointMatcher_ROS::pointMatcherTransformationToRo
 }
 
 template
-geometry_msgs::TransformStamped PointMatcher_ROS::pointMatcherTransformationToRosTf<float>(const PointMatcher<float>::TransformationParameters& inTr,
+geometry_msgs::msg::TransformStamped PointMatcher_ROS::pointMatcherTransformationToRosTf<float>(const PointMatcher<float>::TransformationParameters& inTr,
 																						   const std::string& frame_id,
-																						   const std::string& child_frame_id, const ros::Time& stamp);
+																						   const std::string& child_frame_id, const rclcpp::Time& stamp);
 
 template
-geometry_msgs::TransformStamped PointMatcher_ROS::pointMatcherTransformationToRosTf<double>(const PointMatcher<double>::TransformationParameters& inTr,
+geometry_msgs::msg::TransformStamped PointMatcher_ROS::pointMatcherTransformationToRosTf<double>(const PointMatcher<double>::TransformationParameters& inTr,
 																							const std::string& frame_id,
-																							const std::string& child_frame_id, const ros::Time& stamp);
+																							const std::string& child_frame_id, const rclcpp::Time& stamp);
